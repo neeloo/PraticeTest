4bd492fa251c0ea00da8ace716ff6e8c7d342623
Neeloo patel <nilusingh945164@gmail.com>
2026-07-18 11:01:30 +0530

    Crud APi

--- CRUD-API/src/main/java/com/practice/CRUD/API/CrudApiApplication.java
+++ CRUD-API/src/main/java/com/practice/CRUD/API/CrudApiApplication.java
@@ -9,6 +9,7 @@ public class CrudApiApplication {
 	public static void main(String[] args) {
 
         SpringApplication.run(CrudApiApplication.class, args);
+
         //System.out.println("hello");
 	}
 
