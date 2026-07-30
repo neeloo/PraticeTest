b31c88c44ab2bc677a30b0f2fabca46718724a0f
Neeloo patel <nilusingh945164@gmail.com>
2026-07-30 19:52:46 +0530

    login api

--- PraticeTest/src/main/java/com/neeloo/PraticeTest/LoginApi/model/LoginUser.java
+++ PraticeTest/src/main/java/com/neeloo/PraticeTest/LoginApi/model/LoginUser.java
@@ -1,4 +1,31 @@
 package com.neeloo.PraticeTest.LoginApi.model;
 
 public class LoginUser {
+
+    private String userName;
+    private String password;
+
+    public LoginUser(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+    public  LoginUser(){}
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
 }
